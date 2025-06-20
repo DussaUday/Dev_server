d39539201d525fbb886ff9e08595968e85ad1815
DussaUday <dussauday469@gmail.com>
2025-06-21 00:17:26 +0530

    first commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:3000', 'http://localhost:5173'],
+    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://portfolio-website-tau-azure.vercel.app/'],
     methods: ['GET', 'POST'],
     credentials: true
   }
@@ -30,7 +30,7 @@ if (missingEnvVars.length > 0) {
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://portfolio-website-tau-azure.vercel.app/'],
   credentials: true
 }));
 app.use(express.json());
