33049ad703bd3827764d536b8a2379f3a0cd628c
DussaUday <dussauday469@gmail.com>
2025-06-23 09:08:35 +0530

    /

--- models/Intent.js
+++ models/Intent.js
@@ -4,7 +4,16 @@ const intentSchema = new mongoose.Schema({
   category: String,
   topic: String,
   tags: [String],
-  answer: String
+  answer: [{
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return typeof v === 'string';
+      },
+      message: props => `${props.value} is not a valid string!`
+    }
+  }]
 });
 
 export default mongoose.model('Intent', intentSchema);
