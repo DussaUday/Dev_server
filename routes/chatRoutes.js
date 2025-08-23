88c0d00999aac80e27fbfe37b936cfbf20e013bd
DussaUday <dussauday469@gmail.com>
2025-08-23 06:21:20 +0530

    Update chatRoutes.js

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,12 +1,8 @@
 import express from 'express';
-import { getChatResponse } from '../controllers/chatController.js';
-import rateLimit from 'express-rate-limit';
+import getChatResponse from '../controllers/chatController.js';
+
 const router = express.Router();
-const chatLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 50, // Limit each IP to 50 requests per windowMs
-  message: 'Too many requests from this IP, please try again later'
-});
+
 router.post('/predict', getChatResponse);
 
 export default router;
